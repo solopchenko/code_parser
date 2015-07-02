cb6fe1f2ed78298c99d7c5fcd1b14280183badbf
Svyatoslav Solopchenko <solopchenko@Svyatoslavs-MacBook-Air.local>
2015-07-02 22:42:19 +0600

    [+] Добавлен файл для тестирования работы приложения

---
+++ Code parser/Code parser/Test.cs
@@ -0,0 +1,119 @@
+﻿/*
+ Это файл для тестирования приложения
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//Однострочный комментарий
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Многострочный
+ * комментарий
+ * 1
+ * 2
+ * 3
+ */
+
+namespace Code_parser
+{
+    class Test
+    {
+        //Комментраий в //коментарии
+
+        public void function()
+        {
+            string s = "Обычная строка!";
+
+            try
+            {
+                List<int> k = new List<int>();
+
+                foreach (var item in k)
+                {
+                    while(true)
+                    {
+                        char ch = 'z';
+
+                        switch(ch)
+                        {
+                            case 'm':
+                                {
+                                    break;
+                                }
+                            case 'l':
+                                {
+                                    break;
+                                }
+
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+                    }
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Сообщение");
+            }
+
+            int num = 0;
+            switch (num)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        break;
+                    }
+
+                default:
+                    {
+                        break;
+                    }
+            }
+
+            if (num == 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        string str = "Строка throw throw throw";
+                    }
+
+                    catch
+                    {
+                        return;
+                    }
+                }
+            }
+            else if (num == 10)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
+
+/*
+ * Привет
+ * мир!
+ */
+
+//Комментарий внизу
+
+/* Привет */
