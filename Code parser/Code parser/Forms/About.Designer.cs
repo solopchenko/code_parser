0596f07a9635fcd0254397d3638090b4408111ad
Svyatoslav Solopchenko <solopchenko@Svyatoslavs-MacBook-Air.local>
2015-07-13 22:51:12 +0600

    [*] Copyright

--- Code parser/Code parser/Forms/About.Designer.cs
+++ Code parser/Code parser/Forms/About.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.developer_label = new System.Windows.Forms.Label();
+            this.name_label = new System.Windows.Forms.Label();
+            this.student_label = new System.Windows.Forms.Label();
+            this.university_1_label = new System.Windows.Forms.Label();
+            this.university_2_label = new System.Windows.Forms.Label();
             this.icLogo_pictureBox = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.company_label = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.icLogo_pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
+            // developer_label
+            // 
+            this.developer_label.AutoSize = true;
+            this.developer_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.developer_label.Location = new System.Drawing.Point(133, 12);
+            this.developer_label.Name = "developer_label";
+            this.developer_label.Size = new System.Drawing.Size(103, 18);
+            this.developer_label.TabIndex = 1;
+            this.developer_label.Text = "Разработчик:";
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.name_label.Location = new System.Drawing.Point(242, 14);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(179, 18);
+            this.name_label.TabIndex = 2;
+            this.name_label.Text = "Солопченко Святослав,";
+            // 
+            // student_label
+            // 
+            this.student_label.AutoSize = true;
+            this.student_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.student_label.Location = new System.Drawing.Point(242, 32);
+            this.student_label.Name = "student_label";
+            this.student_label.Size = new System.Drawing.Size(231, 18);
+            this.student_label.TabIndex = 3;
+            this.student_label.Text = "студент Института Кибернетики";
+            // 
+            // university_1_label
+            // 
+            this.university_1_label.AutoSize = true;
+            this.university_1_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.university_1_label.Location = new System.Drawing.Point(242, 50);
+            this.university_1_label.Name = "university_1_label";
+            this.university_1_label.Size = new System.Drawing.Size(268, 18);
+            this.university_1_label.TabIndex = 4;
+            this.university_1_label.Text = "Национального исследовательского";
+            // 
+            // university_2_label
+            // 
+            this.university_2_label.AutoSize = true;
+            this.university_2_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.university_2_label.Location = new System.Drawing.Point(242, 68);
+            this.university_2_label.Name = "university_2_label";
+            this.university_2_label.Size = new System.Drawing.Size(306, 18);
+            this.university_2_label.TabIndex = 5;
+            this.university_2_label.Text = "Томского политехнического университета";
+            // 
             // icLogo_pictureBox
             // 
-            this.icLogo_pictureBox.BackgroundImage = global::Code_parser.Properties.Resources.ic_logo;
+            this.icLogo_pictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("icLogo_pictureBox.BackgroundImage")));
             this.icLogo_pictureBox.InitialImage = null;
             this.icLogo_pictureBox.Location = new System.Drawing.Point(12, 12);
             this.icLogo_pictureBox.Name = "icLogo_pictureBox";
@@ -47,66 +99,28 @@
             this.icLogo_pictureBox.TabIndex = 0;
             this.icLogo_pictureBox.TabStop = false;
             // 
-            // label1
+            // company_label
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(133, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 18);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Разработчик:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(242, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 18);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Солопченко Святослав,";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(242, 32);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(231, 18);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "студент Института Кибернетики";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(242, 50);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(268, 18);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Национального исследовательского";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(242, 68);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(306, 18);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Томского политехнического университета";
+            this.company_label.AutoSize = true;
+            this.company_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.company_label.Location = new System.Drawing.Point(70, 133);
+            this.company_label.Name = "company_label";
+            this.company_label.Size = new System.Drawing.Size(440, 18);
+            this.company_label.TabIndex = 6;
+            this.company_label.Text = "По заданию ФГУП НПО им. С.А. Лавочкина, г. о. Химки, 2015";
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(560, 171);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.company_label);
+            this.Controls.Add(this.university_2_label);
+            this.Controls.Add(this.university_1_label);
+            this.Controls.Add(this.student_label);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.developer_label);
             this.Controls.Add(this.icLogo_pictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -123,10 +137,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox icLogo_pictureBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label developer_label;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.Label student_label;
+        private System.Windows.Forms.Label university_1_label;
+        private System.Windows.Forms.Label university_2_label;
+        private System.Windows.Forms.Label company_label;
     }
 }
